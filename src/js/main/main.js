8cf116772f6b0ee06de7293aab0f84663e7d066d
Rasul1918 <darth.rasul@gmail.com>
2019-09-27 17:23:39 +0400

    added better search,shadow

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -13,8 +13,28 @@ $(document).ready(function(){
       speed: 600,
       arrows: true,
       centerMode: true,
-       prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-chevron-left"></i></button>',
-       nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-chevron-right"></i></button>'
+      prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-chevron-left"></i></button>',
+      nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-chevron-right"></i></button>',
+      responsive: [
+        {
+          breakpoint: 992,
+          settings: {
+            slidesToShow: 5
+          }
+        },
+        {
+          breakpoint: 768,
+          settings: {
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 400,
+          settings: {
+            slidesToShow: 1
+          }
+        }
+      ]
     });
     $(".slick-dots li button").text("");
 });
--- src/js/main/navbar.js
+++ src/js/main/navbar.js
@@ -15,6 +15,22 @@ const navbar = ()=> {
 		$("i.timesIcon").toggleClass("iconBlock");
 
 		$(".searchBox").slideToggle("fast");
+		$(".searchInput").focus();
 	});
+
+	// let $navBar = $('.navbarr');
+ //  	let navPos = $navBar.offset().top;
+  	// let scrollPos = $(window).scrollTop();
+
+
+	// $(window).scroll(function() {
+ //       let scrollPos = $(this).scrollTop();
+
+ //      if (scrollPos >= navPos+40) {
+ //          $navBar.addClass('sticky');
+ //      } else {
+ //          $navBar.removeClass('sticky');
+ //      }
+ //    });
 }
 export default navbar
