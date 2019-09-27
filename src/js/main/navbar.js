af1804b81fcb2a3b839093a10c259d89f52eee7b
Rasul1918 <darth.rasul@gmail.com>
2019-09-28 00:42:16 +0400

    changed class names

--- src/js/main/navbar.js
+++ src/js/main/navbar.js
@@ -1,16 +1,6 @@
 const navbar = ()=> {
-	// let addWidth = () => {
-	// 	document.querySelector(".navbar--logo").classList.add("navLogoSmall");
-	// }
-	// let delWidth = () => {
-	// 	document.querySelector(".navbar--logo").classList.remove("navLogoSmall");
-	// }
-
-	// let searchInput=document.querySelector(".b-search-input");
-		
-	// searchInput.onfocus=function() {addWidth();}
-	// searchInput.onfocusout=function() {delWidth();}  
-	$(".collapse--box__search").click(function(){
+	
+	$(".collapse-box__search").click(function(){
 		$("i.searchIcon").toggleClass("iconNone");
 		$("i.timesIcon").toggleClass("iconBlock");
 
@@ -18,19 +8,5 @@ const navbar = ()=> {
 		$(".searchInput").focus();
 	});
 
-	// let $navBar = $('.navbarr');
- //  	let navPos = $navBar.offset().top;
-  	// let scrollPos = $(window).scrollTop();
-
-
-	// $(window).scroll(function() {
- //       let scrollPos = $(this).scrollTop();
-
- //      if (scrollPos >= navPos+40) {
- //          $navBar.addClass('sticky');
- //      } else {
- //          $navBar.removeClass('sticky');
- //      }
- //    });
 }
 export default navbar
