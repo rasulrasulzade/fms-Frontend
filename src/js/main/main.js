7d1d5d164c9002bd9f0fa90f11bc49e1420e688f
Rasul1918 <darth.rasul@gmail.com>
2019-09-30 15:39:55 +0400

    grayscale to slider

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -12,7 +12,7 @@ $(document).ready(function(){
       autoplay: true,
       speed: 600,
       arrows: true,
-      centerMode: true,
+      // centerMode: true,
       prevArrow: '<button class="slide-arrow prev-arrow"><i class="fa fa-chevron-left"></i></button>',
       nextArrow: '<button class="slide-arrow next-arrow"><i class="fa fa-chevron-right"></i></button>',
       responsive: [
--- src/js/main/navbar.js
+++ src/js/main/navbar.js
@@ -7,6 +7,34 @@ const navbar = ()=> {
 		$(".searchBox").slideToggle("fast");
 		$(".searchInput").focus();
 	});
-
 }
+// const closeNav = ()=> {
+
+	
+// 	const nav = $('.b-nav')
+// 	nav.removeClass('b-nav--active')
+	
+// }
+const documentListener = (cb=null) => {
+	$(document).on('click touchstart', (e)=>{
+		const _this = $(e.target)
+		
+
+		// if not self clicked
+		if(!_this.closest('.searchBox').length && !_this.closest('.collapse-box__search').length){
+			// closeNav()
+			$(".searchBox").hide();
+			$("i.timesIcon").removeClass("iconBlock");
+			$("i.searchIcon").removeClass("iconNone");
+
+		}
+		// if(!_this.closest('.searchInput').length && !_this.closest('.searchBox').length){
+		// 	$('.shopping-card').removeClass('shopping-card--active')
+		// 	$('.searchBox').hide()
+		// }
+		
+	})
+}
+documentListener();
+
 export default navbar
