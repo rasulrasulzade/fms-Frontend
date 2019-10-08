2dad1554724ac4db3827b7e9bdd8dbb9e045020d
Rasul1918 <darth.rasul@gmail.com>
2019-10-08 17:23:26 +0400

    added new pages

--- src/js/main/services.js
+++ src/js/main/services.js
@@ -6,8 +6,9 @@ const servicFunc = ()=> {
 		$('.list-item__header:eq('+i+')').click(function(){
 			$('.list-item__text:eq('+i+')').slideToggle('Block');
 			$('.list-item:eq('+i+')').toggleClass('list-item--active');
-			$('.list-item__plus:eq('+i+')').toggleClass('None');
-			$('.list-item__times:eq('+i+')').toggleClass('Block');
+			// $('.list-item__plus:eq('+i+')').toggleClass('None');
+			// $('.list-item__times:eq('+i+')').toggleClass('Block');
+			$('.list-item__plus:eq('+i+')').toggleClass('plus-times')
 		});
 		
 	}
