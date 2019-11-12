d79e4e1966f2f006266ced8e4e51ac511d6240de
Rasul1918 <darth.rasul@gmail.com>
2019-11-12 12:25:08 +0400

    navbar item changed

--- src/js/main/main.js
+++ src/js/main/main.js
@@ -21,25 +21,4 @@ $(document).ready(function(){
 
  contact__OrangeText();
 
-// ymaps.ready(initMap);
-//         function initMap(){
-//             var myMap = new ymaps.Map("map", {
-//                 // center: [x, y],
-//                 // center: [40.387659, 49.815589],
-//                 zoom: 17,
-//                 // controls: ['zoomControl', 'fullscreenControl']
-//             });
-//               // from FMS
-//                var myPlacemark = new ymaps.Placemark(myMap.getCenter(), {
-//                                           balloonContent: '<strong>blue</strong> color',
-//                                           iconCaption: 'FMS'
-//                                       }, {
-//                                           iconLayout: 'default#image',
-//                                           iconImageHref: 'img/maps-and-flags.png',
-//                                           iconImageSize: [40, 55],
-//                                           // iconImageOffset: [-5, -38]
-//                                       })
-//                                       myMap.geoObjects.add(myPlacemark)
-//             // the end
-//          }
 });
